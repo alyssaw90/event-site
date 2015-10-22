858631ebfec17936abc778c5e32010c15222c182
mb4ms <v-mibowe@MININT-MGBF5CF.redmond.corp.microsoft.com>
2015-10-22 10:17:06 -0700

    move simple view routes into one file

--- server.js
+++ server.js
@@ -15,48 +15,22 @@ var sql = new Sql('events_page', 'eventsUser', 'p@ssw0rd1', {
     idle: 10000
   }
 });*/
-var mapRouter = express.Router();
-var homeRouter = express.Router();
-var latestNewsRouter = express.Router();
-var findEventsRouter = express.Router();
-var pastEventsRouter = express.Router();
-var mediaRouter = express.Router();
-var faqRouter = express.Router();
-var meetTheTeamRouter = express.Router();
-var contactRouter = express.Router();
-var futureEventsRouter = express.Router();
+
 var aboutRouter = express.Router();
 var dbRouter = express.Router();
-require('./routes/home-routes')(homeRouter);
-require('./routes/world-map-routes')(mapRouter);
-require('./routes/latest-news-routes')(latestNewsRouter);
-require('./routes/find-an-event-routes')(findEventsRouter);
-require('./routes/past-events-routes')(pastEventsRouter);
-require('./routes/media-routes')(mediaRouter);
-require('./routes/faq-routes')(faqRouter);
-require('./routes/meet-the-team-routes')(meetTheTeamRouter);
-require('./routes/contact-routes')(contactRouter);
-require('./routes/future-events-routes')(futureEventsRouter);
+var viewRouter = express.Router();
 require('./routes/about-routes')(aboutRouter);
 require('./routes/db-routes')(dbRouter);
+require('./routes/view-routes')(viewRouter);
 
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/'));
-app.use('/', homeRouter);
-app.use('/', mapRouter);
-app.use('/', latestNewsRouter);
-app.use('/', findEventsRouter);
-app.use('/', pastEventsRouter);
-app.use('/', mediaRouter);
-app.use('/', faqRouter);
-app.use('/', meetTheTeamRouter);
-app.use('/', contactRouter);
-app.use('/', futureEventsRouter);
 app.use('/', aboutRouter);
 app.use('/', dbRouter);
+app.use('/', viewRouter);
 
 app.listen(port, function () {
 	console.log('server started on port ' + port + ' at ' + time);
