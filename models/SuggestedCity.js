bba5e5a01c507fc5f1117c785f54ae192cd38d80
mb4ms <v-mibowe@MININT-MGBF5CF.redmond.corp.microsoft.com>
2015-12-16 15:54:40 -0800

    add latest to try and connect to azure db

--- models/SuggestedCity.js
+++ models/SuggestedCity.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Sql = require('sequelize');
-var sql = new Sql('events_page', 'eventsUser', 'p@ssw0rd1', {
+/*var sql = new Sql('events_page', 'eventsUser', 'p@ssw0rd1', {
   host: 'localhost',
   dialect: 'mssql',
 
@@ -10,8 +10,26 @@ var sql = new Sql('events_page', 'eventsUser', 'p@ssw0rd1', {
     min: 0,
     idle: 10000
   }
+});*/
+/*var sql = new Sql('Driver={SQL Server Native Client 11.0};Server=tcp:interopeventstestserver.database.windows.net,1433;Database=InteropEventsDBTest;Uid=EventAdmin@interopeventstestserver;Pwd={Event.4ever!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;', {
+  dialect: 'mssql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
-
+*/
+var sql = new Sql('InteropEventsDBTest', 'EventAdmin', 'Event.4ever!', {
+  host: 'interopeventstestserver.database.windows.net',
+  dialect: 'mssql',
+  port: 1433,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 var SuggestedCity = module.exports = sql.define('SuggestedCity', {
   city: Sql.STRING,
   contactId: Sql.INTEGER
