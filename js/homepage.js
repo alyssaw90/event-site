733a0e8cc8147edcc9907bf3d65db33c64299843
mb4ms <v-mibowe@MININT-MGBF5CF.redmond.corp.microsoft.com>
2015-12-30 15:48:01 -0800

    make backToTop button remove highlight and close opened div from introBlocks

--- js/homepage.js
+++ js/homepage.js
@@ -47,6 +47,7 @@ $(document).ready(function () {
 	var $toHelpYouBlock = $('#toHelpYouBlock');
 	var $whoMadeItBlock = $('#whoMadeItBlock');
 	var $itsYourEventBlock = $('#itsYourEventBlock');
+	var $backToTopButton = $('.scroll-button .fa-chevron-up');
 	var $window = $(window);
 	
 	//randomly assign background-color to the slides -- .slideshow li:nth-child(2) h2:first-child
@@ -94,6 +95,17 @@ $(document).ready(function () {
 			}); 	
 		}
 	})*/
+	//close textbox and unhighlight introBox when $backToTopButton is clicked
+	$backToTopButton.click(function (e) {
+		$('.hiddenHomepageSections:visible').hide();
+		$('.homepageIntroBlocks').each(function (i, elem) {
+			var $this = $(this);
+			var thisBlockMobileClass = $this.attr('id') + 'Mobile'
+			if ($this.hasClass(thisBlockMobileClass)) {
+				$this.removeClass(thisBlockMobileClass);
+			}
+		})
+	})
 	//when a homepageIntroBlocks is clicked, show the corresponding div
 	$homepageIntroBlocks.click(function (e) {
 		var $this = $(this);
