2682df2ec8ca7b220c30a7f44ca81392f1ba2d5d
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-03-17 15:27:09 -0700

    add curriculum in Chinese for Taipei

--- js/curriculum-page.js
+++ js/curriculum-page.js
@@ -2,6 +2,25 @@
 
 $(function () {
 	var $curriculumLogoutButton = $('#curriculumLogoutButton');
+	var $taipei1016Eng = $('.taipei1016-eng');
+	var $taipei2016Cn = $('.taipei2016-cn');
+	var $taipei2016ChineseCurriculum = $('#taipei2016-chinese-curriculum');
+	var $taipei2016EnglishCurriculum = $('#taipei2016-english-curriculum');
+	$taipei2016EnglishCurriculum.hide();
+	$taipei2016ChineseCurriculum.show();
 
-	 $('#curriculumLogoutButton').click(logout);
+	 $curriculumLogoutButton.click(logout);
+
+	 $taipei1016Eng.click(function(e) {
+	 	e.preventDefault();
+	 	$taipei2016ChineseCurriculum.show();
+	 	$taipei2016EnglishCurriculum.hide();
+
+	 });
+	 $taipei1016Eng.click(function(e) {
+	 	e.preventDefault();
+	 	$taipei2016EnglishCurriculum.show();
+	 	$taipei2016ChineseCurriculum.hide();
+	 });
+
 })
