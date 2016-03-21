33bbbfff8c6e1bf44cdfac66a552f7b9b21b68b7
Michael Bowen <Michael Bowen>
2016-03-20 17:29:40 -0700

    add remaining English content and welcome pages

--- js/curriculum-page.js
+++ js/curriculum-page.js
@@ -6,21 +6,44 @@ $(function () {
 	var $taipei2016Cn = $('.taipei2016-cn');
 	var $taipei2016ChineseCurriculum = $('#taipei2016-chinese-curriculum');
 	var $taipei2016EnglishCurriculum = $('#taipei2016-english-curriculum');
-	$taipei2016EnglishCurriculum.hide();
-	$taipei2016ChineseCurriculum.show();
+	var $welcomeTab = $('.welcomeTab');
+	var $curriculumList = $('.curriculumList');
+	var languageSection = window.location.href.slice(-3);
 
-	 $curriculumLogoutButton.click(logout);
+	if (languageSection === '-en') {
+		$taipei2016EnglishCurriculum.show();
+		$taipei2016ChineseCurriculum.hide();
+	} else {
+		$taipei2016EnglishCurriculum.hide();
+		$taipei2016ChineseCurriculum.show();	
+	}
 
-	 $taipei2016Cn.click(function(e) {
-	 	e.preventDefault();
-	 	$taipei2016ChineseCurriculum.show();
-	 	$taipei2016EnglishCurriculum.hide();
 
-	 });
-	 $taipei1016Eng.click(function(e) {
-	 	e.preventDefault();
-	 	$taipei2016EnglishCurriculum.show();
-	 	$taipei2016ChineseCurriculum.hide();
-	 });
+ 	$curriculumLogoutButton.click(logout);
+	
+ 	$taipei2016Cn.click(function(e) {
+ 		e.preventDefault();
+ 		$taipei2016ChineseCurriculum.show();
+ 		$taipei2016EnglishCurriculum.hide();
+	
+ 	});
+ 	$taipei1016Eng.click(function(e) {
+ 		e.preventDefault();
+ 		$taipei2016EnglishCurriculum.show();
+ 		$taipei2016ChineseCurriculum.hide();
+ 	});
+	
+ 	$welcomeTab.click(function(e) {
+ 		e.preventDefault();
+ 		var tabid = $(this).data('tabid');
+ 		$curriculumList.each(function(i, elem) {
+ 			var that = $(this);
+ 			that.children().each(function(index, element) {
+ 				if ($(this).attr('id') === tabid) {
+ 					$(this).children().trigger('click');
+ 				}
+ 			});
+ 		});
+ 	});
 
-})
+});
