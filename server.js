ec94c44ea3ca753aa44b53189345b422dd8a8741
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-04-14 13:49:47 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 'use strict';
 
+// require('dotenv').load();
 var express = require('express');
 var app = express();
 var passport = require('passport');
 var clc = require('cli-color');
 var port = process.env.PORT || 3000;
 var time = new Date();
-var fs = require('fs');
-var testEnv = fs.statSync('./.env');
 process.env.SECRET_KEY = process.env.SECRET_KEY || 'change this change this change this!!!';
-console.log(clc.magenta('process.env.SECRET_KEY ::::::::::::::  '), testEnv, clc.magenta('    ::::::     '), testEnv.isFile());
-if (testEnv.isFile()) {
-	// require('dotenv').load();
-	console.log(clc.greenBright('file found!!!!'));
-}
-
 
 app.use(passport.initialize());
 
@@ -27,6 +20,7 @@ require('./routes/db-routes')(dbRouter);
 require('./routes/auth-routes')(authRouter, passport);
 
 
+console.log(clc.magenta('process.env.SECRET_KEY ::::::::::::::  '), process.env.SECRET_KEY);
 
 app.use(express.static(__dirname + '/'));
 
