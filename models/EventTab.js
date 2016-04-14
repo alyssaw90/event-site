662e49ba3070a2c62879e1fd22f47284e1a68424
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-04-14 07:57:37 -0700

    update tabs

--- models/EventTab.js
+++ models/EventTab.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var Sql = require('sequelize');
-var sql = new Sql(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USER, process.env.DB_LOCAL_PASS, {
+/*var sql = new Sql(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USER, process.env.DB_LOCAL_PASS, {
   host: process.env.DB_LOCAL_HOST,
   dialect: 'mssql',
 
@@ -12,8 +12,8 @@ var sql = new Sql(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USER, process.
     min: 0,
     idle: 10000
   }
-});
-/*var sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+});*/
+var sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mssql',
   pool: {
@@ -24,7 +24,7 @@ var sql = new Sql(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USER, process.
   dialectOptions: {
     encrypt: true
   }
-});*/
+});
 
 /*var sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
   host: process.env.DB_DEV_HOST,
@@ -1137,7 +1137,6 @@ $(this).addClass("current");\
 $(tab_next).show();\
 $(this).parent().parent().children("div").hide();\
 $(this).parent().parent().children(divWithTabId).show();\
-console.log($(this).parent().parent().children(divWithTabId), "    ::::::      ", divWithTabId.slice(0, -3), "    ::::::::    ", tabIDName);\
 return false;\
 }\
 });\
