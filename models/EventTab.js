d878ed9128a7a441ec1e04f180a8d4afb7f85141
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-04-19 09:02:52 -0700

    add space at bottom of material tab

--- models/EventTab.js
+++ models/EventTab.js
@@ -13,21 +13,8 @@ var Sql = require('sequelize');
     idle: 10000
   }
 });*/
-/*var sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+var sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'mssql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  dialectOptions: {
-    encrypt: true
-  }
-});*/
-
-var sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
-  host: process.env.DB_DEV_HOST,
   dialect: 'mssql',
   pool: {
     max: 5,
@@ -39,6 +26,19 @@ var sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.
   }
 });
 
+/*var sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
+  host: process.env.DB_DEV_HOST,
+  dialect: 'mssql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  dialectOptions: {
+    encrypt: true
+  }
+});
+*/
 
 var EventTab = module.exports = sql.define('EventTab', {
   eventId: Sql.INTEGER,
