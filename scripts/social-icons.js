67b041db45836544b45ea9b728f64e3eb39d40fb
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-07-14 15:37:51 -0700

    create new social sharing buttons to fix bug 835394

---
+++ scripts/social-icons.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function($) {
+	$(function() {
+
+		 $("#socialSharingIcons").jsSocials({
+      shares: ["twitter", "facebook", "linkedin", "email"],
+      showLabel: false,
+      showCount: false,
+      shareIn: "popup",
+      css: "jsSocialIcons"
+    });
+    $('.jssocials-share-link').each(function(i, el) {
+      var $this = $(this);
+      var socialSite = $this.parent(".jssocials-share").attr('class').replace("jssocials-share jssocials-share-", "");
+      $this.attr("title", "Share to " + socialSite);
+    });
+
+	})
+})(jQuery);
