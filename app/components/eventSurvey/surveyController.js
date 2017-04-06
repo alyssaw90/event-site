01a7d625e75c9496665a4ff699b0265215d40c06
Alyssa <alyssaw90@gmail.com>
2017-04-06 16:00:26 -0700

    survey form working

--- app/components/eventSurvey/surveyController.js
+++ app/components/eventSurvey/surveyController.js
@@ -2,36 +2,57 @@
 
 const swal = require('sweetalert');
 const surveyController = (app) => {
-    app.controller('surveyController', ['$scope', 'surveyRESTResource', '$location', '$window', ($scope, resource, $location)=> {
+    app.controller('surveyController', ['$scope', '$http','surveyRESTResource', '$location', '$window', ($scope, $http, resource, $location)=> {
         
         $scope.newSurvey = {};
-        let createSurveyREST = resource();
-        $scope.checked = true;
+        $scope.hide=true
+        $scope.hide = function(){
+            $scope.hide=false
+        }
+
+        $scope.thankYouHide = false;
+        $scope.thankYouShow = function(){
+            $scope.thankYouHide = true;
+        }
+        
         $scope.createSurvey = (newSurveyData) => {
+            $http({
+                method:'POST',
+                url: '/api/survey',
+                data: newSurveyData
+            }).then(function (response){
+                    // alert('submitted')
+                    // $scope.newSurvey={}
+                    console.log(response.status)
+                    console.log(response.data)
+                }, function(err){
+                    console.log(err)
+                })
             // console.log(newSurveyData)
-            createSurveyREST.createSurvey(newSurveyData, (err, data) => {
-                if(err){
-                    // console.log(err)
-                    swal({
-                        title: 'There was a problem submitting your form',
-                        text: err,
-                        type: 'warning',
-                        confirmButtonColor: '#DD6B55',
-                        confirmButtonText: 'Ok',
-                        customClass: 'sweet-alert-hide-input'
-                    });
-                } 
+            // createSurveyREST.createSurvey(newSurveyData, (err, data) => {
+            //     console.log('this is working!!! :D')
+            //     if(err){
+            //         // console.log(err)
+            //         swal({
+            //             title: 'There was a problem submitting your form',
+            //             text: err,
+            //             type: 'warning',
+            //             confirmButtonColor: '#DD6B55',
+            //             confirmButtonText: 'Ok',
+            //             customClass: 'sweet-alert-hide-input'
+            //         });
+            //     } 
                 
-                if(data.status=="success") {
-                    swal({
-                        title:'Form submitted',
-                        type: 'success'
-                    })
-                    $location.url('/')
-                }
+            //     if(data.status=="success") {
+            //         swal({
+            //             title:'Form submitted',
+            //             type: 'success'
+            //         })
+            //         $location.url('/')
+            //     }
 
                 
-            })
+            // })
         }
     }])
 }
