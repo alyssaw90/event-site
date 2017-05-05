96529fd421133615cdddf6669fc8ea2d065d0fb1
Alyssa <alyssaw90@gmail.com>
2017-05-05 15:15:18 -0700

    working on technology carousel media queries

--- app/components/homepage/HomepageCtrl.js
+++ app/components/homepage/HomepageCtrl.js
@@ -7,6 +7,37 @@ const HomepageCtrl = (app) => {
 	   return pageUrl === $location.path();
 		}
 
+		 $scope.miniGalleryResponsive = [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 980,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 826,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 650,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+    	];
+
 	}])
 }
 
