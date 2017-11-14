6c879ca7f76c24df6bc60f28ff25b1e0a2fa64c7
Ray Dixon (Wicresoft North America) <v-radixo@microsoft.com>
2017-11-13 18:31:47 -0800

    resolving backlog items 1991515, 1991571, and 1991623

--- models/index.js
+++ models/index.js
@@ -31,21 +31,21 @@ let Sql = require('sequelize');
 });*/
 
 //DEVELOPMENT/TEST DATABASE
-/*let sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
-  host: process.env.DB_DEV_HOST,
-  dialect: 'mssql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  dialectOptions: {
-    encrypt: true
-  }
-});*/
+let sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
+    host: process.env.DB_DEV_HOST,
+    dialect: 'mssql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    dialectOptions: {
+        encrypt: true
+    }
+});
 
 //LIVE DATABASE
-let sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+/* let sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mssql',
   pool: {
@@ -56,22 +56,22 @@ let sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,
   dialectOptions: {
     encrypt: true
   }
-});
+}); */
 
 //load models
 let models = ['Speaker', 'Event', 'EventTab', 'User', 'Slideshow', 'EventSpeakersAsscs', 'Slide', 'SlideshowSlideAssc', 'MsUser', 'Surveys'];
 
 models.forEach(function(model) {
-  module.exports[model] = sql.import(__dirname + '/' + model);
+    module.exports[model] = sql.import(__dirname + '/' + model);
 });
 //create model associations
 sql.sync()
-sql.models.Event.belongsToMany(sql.models.Speaker, {through: sql.models.EventSpeakersAsscs});
-sql.models.Speaker.belongsToMany(sql.models.Event, {through: sql.models.EventSpeakersAsscs});
-sql.models.Event.belongsToMany(sql.models.EventTab, {through: 'EventsTabsAccsc'});
-sql.models.EventTab.belongsToMany(sql.models.Event, {through: 'EventsTabsAccsc'});
-sql.models.Slide.belongsToMany(sql.models.Slideshow, {through: sql.models.SlideshowSlideAssc});
-sql.models.Slideshow.belongsToMany(sql.models.Slide, {through: sql.models.SlideshowSlideAssc});
+sql.models.Event.belongsToMany(sql.models.Speaker, { through: sql.models.EventSpeakersAsscs });
+sql.models.Speaker.belongsToMany(sql.models.Event, { through: sql.models.EventSpeakersAsscs });
+sql.models.Event.belongsToMany(sql.models.EventTab, { through: 'EventsTabsAccsc' });
+sql.models.EventTab.belongsToMany(sql.models.Event, { through: 'EventsTabsAccsc' });
+sql.models.Slide.belongsToMany(sql.models.Slideshow, { through: sql.models.SlideshowSlideAssc });
+sql.models.Slideshow.belongsToMany(sql.models.Slide, { through: sql.models.SlideshowSlideAssc });
 
 
 //export connection
