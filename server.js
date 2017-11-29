601812d08cf0d86c35d4afda57476878bee44dc9
Ray Dixon <v-radixo@microsoft.com>
2017-11-29 14:18:18 -0800

    adding skeleton file for admin routes and updating server.js to support it - 2019975 Refactor API routes for admin portal - https://office.visualstudio.com/OC/DevX_DXIC_InteropSupport/_workitems/edit/2019975

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const cookieParser = require('cookie-parser');
 const models = require('./models');
 const bodyParser = require('body-parser');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const adminRoutes = express.Router();
 const apiRoutes = express.Router();
 const authRoutes = express.Router();
 const catchAllRoutes = express.Router();
@@ -18,15 +19,16 @@ const auth = express.Router()
 process.env.SECRET_KEY = process.env.SECRET_KEY || 'change this change this change this!!!';
 let port = process.env.PORT || 3000;
 let time = new Date();
-let secretKeyReminder = process.env.SECRET_KEY === 'change this change this change this!!!' ?  clc.black.bgRed(`process.env.SECRET_KEY is not secure, change your SECRET_KEY!!!`) : clc.black.bgGreen(`Your SECRET_KEY is secure. You don't need to change your SECRET_KEY`);
+let secretKeyReminder = process.env.SECRET_KEY === 'change this change this change this!!!' ? clc.black.bgRed(`process.env.SECRET_KEY is not secure, change your SECRET_KEY!!!`) : clc.black.bgGreen(`Your SECRET_KEY is secure. You don't need to change your SECRET_KEY`);
 console.log(secretKeyReminder);
 
+require('./routes/admin-routes')(adminRoutes);
 require('./routes/api-routes')(apiRoutes);
 require('./routes/auth.js')(auth, passport)
 require('./routes/catch-all-routes.js')(catchAllRoutes);
 
 // Configure requests parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(compression());
@@ -56,7 +58,7 @@ passport.use(new OIDCBearerStrategy({
     "identityMetadata": process.env.AZURE_METADATA,
     "clientID": process.env.AZURE_CLIENT_ID,
     "validateIssuer": false,
-}, function(token, done){
+}, function(token, done) {
     return done(null, token, null);
 }))
 
@@ -67,18 +69,18 @@ app.use((req, res, next) => {
 });
 // use the root directory as the source of static files
 app.use(express.static(__dirname + '/'))
-// use the apiRoutes with /api/ as its root
+    // use the apiRoutes with /api/ as its root
 app.use('/api/', apiRoutes);
+// use the adminRoutes with /api/ as its root
+app.use('/api/', adminRoutes);
 //use the root for the catch all route, this must be the last routes file in the list above
 app.use('/', catchAllRoutes);
 // Error handling
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-})
-// listen to the port and log when the server has started
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    })
+    // listen to the port and log when the server has started
 app.listen(port, () => {
     console.log(clc.cyanBright('server started on port ' + port + ' at ' + time));
 })
-
-
