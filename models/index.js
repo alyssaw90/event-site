e47c2fc36b9d950806f8fb6b1d54a3ca4a3eec1c
Alyssa <alyssaw90@gmail.com>
2017-12-22 12:38:55 -0800

    change database to live database

--- models/index.js
+++ models/index.js
@@ -31,7 +31,7 @@ let Sql = require('sequelize');
 });*/
 
 //DEVELOPMENT/TEST DATABASE
-let sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
+/*let sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.DB_DEV_PASS, {
     host: process.env.DB_DEV_HOST,
     dialect: 'mssql',
     pool: {
@@ -42,10 +42,10 @@ let sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.
     dialectOptions: {
         encrypt: true
     }
-});
+});*/
 
 //LIVE DATABASE
-/* let sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+let sql = new Sql(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mssql',
   pool: {
@@ -56,7 +56,7 @@ let sql = new Sql(process.env.DB_DEV_NAME, process.env.DB_DEV_USER, process.env.
   dialectOptions: {
     encrypt: true
   }
-}); */
+}); 
 
 //load models
 let models = ['Speaker', 'Event', 'EventTab', 'EventSpeakersAsscs', 'Surveys'];
